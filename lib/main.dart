b3a4f1db4298f136655c3066b9c4e25b93e66cb1
Galileo Teco <g@80bits.com>
2020-08-09 18:25:20 -0500

    improved

--- lib/main.dart
+++ lib/main.dart
@@ -79,52 +79,59 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Container(
-        margin: EdgeInsets.only(left: 10, right: 10),
+        color: Color(0xFFF4F4F4),
         child: Center(
           // Center is a layout widget. It takes a single child and positions it
           // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              NButton(
-                text: 'Click',
-                onPressed: _incrementCounter,
-                type: 1,
-              ),
-              SizedBox(height: 10,),
-              NButton(
-                text: 'Click',
-                onPressed: _incrementCounter,
-                type: 2,
-              ),
-              SizedBox(height: 10,),
-              NButton(
-                text: 'Click',
-                onPressed: _incrementCounter,
-                type: 3,
-              ),
-            ],
+          child: Container(
+            margin: EdgeInsets.only(left:25,right:25),
+            child: Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Invoke "debug painting" (press "p" in the console, choose the
+              // "Toggle Debug Paint" action from the Flutter Inspector in Android
+              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+              // to see the wireframe for each widget.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                NButton(
+                  text: 'Click',
+                  onPressed: _incrementCounter,
+                  type: 1,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                NButton(
+                  text: 'Click',
+                  onPressed: _incrementCounter,
+                  type: 2,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                NButton(
+                  text: 'Click',
+                  onPressed: _incrementCounter,
+                  type: 3,
+                ),
+              ],
+            ),
           ),
         ),
       ),
